dd2dccc18fc634e951068b5a46e94a636ab446d2
Dammy Knight <olaoluwaayomide10@gmail.com>
2023-09-24 21:22:39 +0100

    State update batching in practice

--- src/App.js
+++ src/App.js
@@ -65,7 +65,25 @@ function TabContent({ item }) {
 	const [likes, setLikes] = useState(0);
 
 	function handleInc() {
-		setLikes(likes + 1);
+		setLikes((likes) => likes + 1);
+	}
+	function handleTripleInc() {
+		// setLikes(likes + 1);
+		// setLikes(likes + 1);
+		// setLikes(likes + 1);
+
+		setLikes((likes) => likes + 1);
+		setLikes((likes) => likes + 1);
+		setLikes((likes) => likes + 1);
+	}
+
+	function handleUndo() {
+		setShowDetails(true);
+		setLikes(0);
+	}
+
+	function handleUndoLater() {
+		setTimeout(handleUndo, 2000);
 	}
 
 	return (
@@ -81,13 +99,13 @@ function TabContent({ item }) {
 				<div className='hearts-counter'>
 					<span>{likes} ❤️</span>
 					<button onClick={handleInc}>+</button>
-					<button>+++</button>
+					<button onClick={handleTripleInc}>+++</button>
 				</div>
 			</div>
 
 			<div className='tab-undo'>
-				<button>Undo</button>
-				<button>Undo in 2s</button>
+				<button onClick={setShowDetails}>Undo</button>
+				<button onClick={handleUndoLater}>Undo in 2s</button>
 			</div>
 		</div>
 	);
